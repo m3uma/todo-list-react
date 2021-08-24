39cd817aa13637dbd07291a4e55807e7ecad1b12
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-08-24 19:25:44 +0200

    addNewTask interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ])
+  };
+
   return (
     <Container>
       <Header title="TO DO LIST" />
       <Section
         title="Add new task..."
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__input" placeholder="What should I do?" />
-        <button className="form__button">Add task</button>
-    </form>
-)
+const Form = ({addNewTask}) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    }
+    
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="What should I do?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Add task</button>
+        </form>
+    )
+}
 
 export default Form;
