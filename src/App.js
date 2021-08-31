0618615fd908bc06f0f357da60081b3d5b1bd9fd
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-08-31 18:30:24 +0200

    add more pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import Tasks from "./features/tasks/Tasks";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import { StyledNavLink } from "./styled";
 
-const App = () => <Tasks />;
+const App = () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <StyledNavLink to="/tasks">Tasks</StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to="/author">About author</StyledNavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/tasks/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/tasks">
+                    <TasksPage />
+                </Route>
+                <Route path="/author">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/tasks" />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
 
 export default App;
 
