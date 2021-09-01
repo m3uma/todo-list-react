469e1044d25822f94863db5506d54591fea8e8f7
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-09-01 17:05:22 +0200

    separated download button

---
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
+import { Button } from "../Button";
+
+const FetchExampleTasksButton = () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(selectLoading);
+    return (
+        <Button disabled={loading} onClick={() => dispatch(fetchExampleTasks())}>
+            {
+                loading
+                    ? "Loading..."
+                    : "Download sample tasks"
+            }
+        </Button>
+    );
+};
+
+export default FetchExampleTasksButton;
